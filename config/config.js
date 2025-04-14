6b99c5d88d9ae151cf6948b37df6481467eee2c0
theboywidsmile <manirud0077@gmail.com>
2025-04-14 23:28:34 +0530

    feat: fix production configuration and add SSL options for development environment

--- config/config.js
+++ config/config.js
@@ -22,11 +22,18 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
-  production: {
+  development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
