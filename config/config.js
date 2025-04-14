4560c91f0bb686fa61b41534a2cb73d997860227
theboywidsmile <manirud0077@gmail.com>
2025-04-14 23:33:14 +0530

    feat: update production configuration to include database settings

--- config/config.js
+++ config/config.js
@@ -22,7 +22,7 @@ module.exports = {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
   },
-  development: {
+  production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
